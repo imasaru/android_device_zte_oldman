0e6f7577730ec77768d4b6a13a9af36473eb1117
imasaru <evanmr4@gmail.com>
2017-09-03 00:59:08 -0500

    Rename and Update Android.mk

--- Android.mk
+++ Android.mk
@@ -6,3 +6,7 @@ ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
 
 # include the non-open-source counterpart to this file
 -include vendor/zte/oldman/AndroidBoardVendor.mk
+
+ifeq ($(TARGET_DEVICE),oldman)
+include $(call all-makefiles-under,$(LOCAL_PATH))
+endif
